a40ed0d6d3dae013cc4b0cef40f594c443b4e551
Nick Dubrov <nickdubrov1992@gmail.com>
2024-07-18 18:28:35 +0300

    task 4_2

---
+++ chapter_4/task 4_02.py
@@ -0,0 +1,13 @@
+# Вправа по програмуванню 4-2
+# Спалені калорії
+
+# Іменовані константи.
+CALORIES_PER_MINUTE = 4.2
+
+# Виводимо результати на екран.
+print("Хвилини\t\tСпалені калоріЇ")
+print("-------------------------------")
+
+for minutes in range(5, 31, 5):
+    calories = minutes * CALORIES_PER_MINUTE
+    print(minutes, "\t\t", calories)
