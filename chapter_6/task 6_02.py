3742d00723b05da0f91dcb1c412097a55275f314
Nick Dubrov <nickdubrov1992@gmail.com>
2025-03-14 13:21:34 +0200

    task 6_02

---
+++ chapter_6/task 6_02.py
@@ -0,0 +1,31 @@
+# Вправа по програмуванню 6-2
+# Виведення на екран верхньої частини файлу
+
+
+# Головна функція.
+def main():
+    # Отримання назви файлу.
+    filename = input("Назва файлу: ")
+
+    # Відкриття файлу для читання.
+    infile = open(
+        r"E:\Python\TGaddis_Python_course\chapter_6\data\\" + filename)
+
+    # Первинне читання файлу.
+    line = infile.readline()
+    i = 1
+
+    # Читаємо файл та виводимо результати на екран.
+    while line != '' and i <= 5:
+        line = line.rstrip('\n')
+        print(line)
+
+        line = infile.readline()
+        i += 1
+
+    # Закриття файлу.
+    infile.close()
+
+
+# Виклик головної функції.
+main()
