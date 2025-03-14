7288413ac5b67ecc43cbf2e55a759c06669c56e2
Nick Dubrov <nickdubrov1992@gmail.com>
2025-03-14 14:00:20 +0200

    task 6_03

--- chapter_6/task 6_01.py
+++ chapter_6/task 6_01.py
@@ -5,8 +5,7 @@
 # Головна функція.
 def main():
     # Відкриття файлу для читання.
-    infile = open(
-        r"E:\Python\TGaddis_Python_course\chapter_6\data\numbers.txt")
+    infile = open(r"chapter_6\data\numbers.txt")
 
     # Читання файлу та збереження його вмісту в змінній.
     contents = infile.read()
