0f9b00b0d3fc2a420f5c495a69e17d8c05eae609
Nick Dubrov <nickdubrov1992@gmail.com>
2025-03-24 13:48:01 +0200

    task 6_10

---
+++ chapter_6/task 6_10a.py
@@ -0,0 +1,26 @@
+# Вправа по програмуванню 6-10а
+# Рахунок у грі в гольф
+
+
+# Головна функція.
+def main():
+    # Отримання кількості гравців в турнірі.
+    num_players = int(input("Кількість гравців: "))
+
+    # Відкриття файлу для запису.
+    outfile = open("chapter_6\\data\\golf.txt", 'w')
+
+    # Отримання даних про гравця та запис їх до файлу.
+    for i in range(num_players):
+        player = input("\nІм'я гравця: ")
+        score = input("Рахунок гравця: ")
+
+        outfile.write(player + '\n')
+        outfile.write(score + '\n')
+
+    # Закриття файлу.
+    outfile.close()
+
+
+# Виклик головної функції.
+main()
